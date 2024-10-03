e45076e8d1562bdcac99ec86fb57565ce9dc973c
Steddy <qwesteddyoff@gmail.com>
2024-10-03 12:27:08 +0300

    edit random recipe

--- src/components/RandomRecipe/RandomRecipe.jsx
+++ src/components/RandomRecipe/RandomRecipe.jsx
@@ -8,8 +8,10 @@ const RandomRecipe = () => {
       <div className={cl.random_wrapper}>
         <div className={cl.wrapper_title}>Случайный рецепт</div>
         <img src="" alt="randomRecipe" className={cl.random_dish} />
-        <p className={cl.random_title}>Название</p>
-        <p className={cl.random_country}>Кухня</p>
+        <div className={cl.wapper_inner}>
+          <p className={cl.random_title}>Название</p>
+          <p className={cl.random_country}>Кухня</p>
+        </div>
       </div>
     </div>
   );
