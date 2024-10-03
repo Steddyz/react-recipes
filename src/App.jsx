2eaad8a8f4a59f821743c3b01638058d09d30512
Steddy <qwesteddyoff@gmail.com>
2024-10-03 12:18:16 +0300

    add ingredient search

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function App() {
     <>
       <Header />
       <RandomRecipe />
-      {/* <IngredientsForm />
-      <Footer /> */}
+      <IngredientsForm />
+      {/* <Footer /> */}
     </>
   );
 }
--- src/components/IngredientsForm/IngredientsForm.jsx
+++ src/components/IngredientsForm/IngredientsForm.jsx
@@ -3,7 +3,30 @@ import React from "react";
 import cl from "./IngredientsForm.module.css";
 
 const IngredientsForm = () => {
-  return <div>IngredientsForm</div>;
+  return (
+    <div className={cl.ingredients_title}>
+      <h1 className={cl.title}>Поиск по ингредиентам</h1>
+      <hr />
+      <form className={cl.ingredients_form}>
+        <div className={cl.form_group}>
+          <label className={cl.form_label}>Желаемые ингредиенты </label>
+          <input
+            className={cl.form_input}
+            placeholder="Введите желаемые ингредиенты"
+          />
+        </div>
+        <div className="form-group">
+          <label className={cl.form_label}>Нежелаемые ингредиенты</label>
+
+          <input
+            className={cl.form_input}
+            placeholder="Введите нежелаемые ингредиенты"
+          />
+        </div>
+        <button className={cl.form_button}>Отправить</button>
+      </form>
+    </div>
+  );
 };
 
 export default IngredientsForm;
