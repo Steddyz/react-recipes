d3f23e27c933e59c353264882f54b93c02c92615
Steddy <qwesteddyoff@gmail.com>
2024-10-05 09:30:34 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import "./App.css";
 import Header from "./components/Header/Header";
 import RandomRecipe from "./components/RandomRecipe/RandomRecipe";
@@ -7,14 +5,12 @@ import Footer from "./components/Footer/Footer";
 import IngredientsForm from "./components/IngredientsForm/IngredientsForm";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
       <RandomRecipe />
       <IngredientsForm />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
