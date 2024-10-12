97b6205878322f7e240f00ae6f117048a33bfe31
Steddy <qwesteddyoff@gmail.com>
2024-10-12 12:57:01 +0300

    add flags + countrie page

--- src/pages/CountriePage/CountriePage.jsx
+++ src/pages/CountriePage/CountriePage.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+import cl from "./CountriePage.module.css";
 
 const CountriePage = () => {
-  return <div>CountriePage</div>;
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const allCountries = async () => {
+      setLoading(true);
+      const response = await axios.get(
+        "https://www.themealdb.com/api/json/v1/1/list.php?a=list"
+      );
+      console.log(response.data.meals);
+
+      setCountries(response.data.meals);
+      setLoading(false);
+    };
+    allCountries();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <div className={cl.loading}>Загрузка...</div>
+      ) : (
+        <div className={cl.countrie}>
+          {countries.map((countrie, index) => (
+            <div key={index} className={cl.countrie_wrapper}>
+              <img src="" alt={countrie.strArea} />
+              <p>{countrie.strArea}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default CountriePage;
