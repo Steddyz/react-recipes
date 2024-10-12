d345ebe4642709e109a7a4c821479a9c35a3b6cb
Steddy <qwesteddyoff@gmail.com>
2024-10-12 18:50:11 +0300

    commit

--- src/pages/CountriePage/CountriePage.jsx
+++ src/pages/CountriePage/CountriePage.jsx
@@ -1,11 +1,72 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
+import American from "../../assets/flags/American.png";
+import British from "../../assets/flags/British.png";
+import Canadian from "../../assets/flags/Canadian.png";
+import Chinese from "../../assets/flags/Chinese.png";
+import Croatian from "../../assets/flags/Croatian.png";
+import Dutch from "../../assets/flags/Dutch.png";
+import Egyptian from "../../assets/flags/Egyptian.png";
+import Filipino from "../../assets/flags/Filipino.png";
+import French from "../../assets/flags/French.png";
+import Greek from "../../assets/flags/Greek.png";
+import Indian from "../../assets/flags/Indian.png";
+import Irish from "../../assets/flags/Irish.png";
+import Italian from "../../assets/flags/Italian.png";
+import Jamaican from "../../assets/flags/Jamaican.png";
+import Japanese from "../../assets/flags/Japanese.png";
+import Kenyan from "../../assets/flags/Kenyan.png";
+import Malaysian from "../../assets/flags/Malaysian.png";
+import Mexican from "../../assets/flags/Mexican.png";
+import Moroccan from "../../assets/flags/Moroccan.png";
+import Polish from "../../assets/flags/Polish.png";
+import Portuguese from "../../assets/flags/Portuguese.png";
+import Russian from "../../assets/flags/Russian.png";
+import Spanish from "../../assets/flags/Spanish.png";
+import Thai from "../../assets/flags/Thai.png";
+import Tunisian from "../../assets/flags/Tunisian.png";
+import Turkish from "../../assets/flags/Turkish.png";
+import Ukrainian from "../../assets/flags/Ukrainian.png";
+import Vietnamese from "../../assets/flags/Vietnamese.png";
+import Unknown from "../../assets/flags/Unknown.png";
+
 import cl from "./CountriePage.module.css";
 
 const CountriePage = () => {
   const [countries, setCountries] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const flagMap = {
+    American: American,
+    British: British,
+    Canadian: Canadian,
+    Chinese: Chinese,
+    Croatian: Croatian,
+    Dutch: Dutch,
+    Egyptian: Egyptian,
+    Filipino: Filipino,
+    French: French,
+    Greek: Greek,
+    Indian: Indian,
+    Irish: Irish,
+    Italian: Italian,
+    Jamaican: Jamaican,
+    Japanese: Japanese,
+    Kenyan: Kenyan,
+    Malaysian: Malaysian,
+    Mexican: Mexican,
+    Moroccan: Moroccan,
+    Polish: Polish,
+    Portuguese: Portuguese,
+    Russian: Russian,
+    Spanish: Spanish,
+    Thai: Thai,
+    Tunisian: Tunisian,
+    Turkish: Turkish,
+    Ukrainian: Ukrainian,
+    Vietnamese: Vietnamese,
+  };
 
   useEffect(() => {
     const allCountries = async () => {
@@ -29,8 +90,12 @@ const CountriePage = () => {
         <div className={cl.countrie}>
           {countries.map((countrie, index) => (
             <div key={index} className={cl.countrie_wrapper}>
-              <img src="" alt={countrie.strArea} />
-              <p>{countrie.strArea}</p>
+              <img
+                src={flagMap[countrie.strArea] || Unknown}
+                alt={countrie.strArea}
+                className={cl.flag_image}
+              />
+              <p className={cl.countrie_name}>{countrie.strArea}</p>
             </div>
           ))}
         </div>
