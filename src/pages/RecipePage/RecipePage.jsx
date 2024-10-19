2549216f1b3c580d2026115e02b1c30fc14fb1ff
Steddy <qwesteddyoff@gmail.com>
2024-10-19 13:57:56 +0300

    add styles

--- src/pages/RecipePage/RecipePage.jsx
+++ src/pages/RecipePage/RecipePage.jsx
@@ -23,29 +23,40 @@ const RecipePage = () => {
   }
 
   return (
-    <div className={cl.recipe_wrapper}>
-      <img
-        className={cl.recipe_image}
-        src={recipe.strMealThumb}
-        alt={recipe.strMeal}
-      />
-      <div>
+    <div className={cl.recipe}>
+      <div className={cl.recipe_wrapper}>
         <h2 className={cl.recipe_title}>{recipe.strMeal}</h2>
-        <p className={cl.recipe_countrie}>Страна: {recipe.strArea}</p>
-        <p className={cl.recipe_category}>Категория: {recipe.strCategory}</p>
-        <p className={cl.recipe_instruction}>
-          Инструкция: {recipe.strInstructions}
-        </p>
+        <div className={cl.options_wrapper}>
+          <img
+            className={cl.recipe_image}
+            src={recipe.strMealThumb}
+            alt={recipe.strMeal}
+          />
+          <div className={cl.options}>
+            <p className={cl.recipe_countrie}>Страна: {recipe.strArea}</p>
+            <p className={cl.recipe_category}>
+              Категория: {recipe.strCategory}
+            </p>
+          </div>
+        </div>
+        <hr />
+
+        <div className={cl.ingredients_wrapper}>
+          <h2 className={cl.ingredient_title}>Ингредиенты</h2>
+
+          <ul className={cl.ingredients_items}>
+            {ingredients.map((item, index) => (
+              <li className={cl.ingredients_item} key={index}>
+                {item.ingredient} {item.measure}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div>
-        <h3>Ингредиенты</h3>
-        <ul>
-          {ingredients.map((item, index) => (
-            <li ley={index}>
-              {item.ingredient} {item.measure}
-            </li>
-          ))}
-        </ul>
+
+      <div className={cl.instruction}>
+        <h3 className={cl.instruction_title}>Инструкция</h3>
+        <p className={cl.recipe_instruction}>{recipe.strInstructions}</p>
       </div>
     </div>
   );
