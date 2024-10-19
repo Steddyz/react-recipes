0fc273a925104f88c82097cdf10799cd45ae36fe
Steddy <qwesteddyoff@gmail.com>
2024-10-19 10:26:34 +0300

    add ingredients + measure

--- src/pages/RecipePage/RecipePage.jsx
+++ src/pages/RecipePage/RecipePage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 
+import cl from "./RecipePage.module.css";
+
 const RecipePage = () => {
   const location = useLocation();
   const recipe = location.state;
@@ -9,13 +11,42 @@ const RecipePage = () => {
     return <div>Загрузка...</div>;
   }
 
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    const ingredient = recipe[`strIngredient${i}`];
+    const measure = recipe[`strMeasure${i}`];
+
+    if (ingredient) {
+      ingredients.push({ ingredient, measure });
+    }
+  }
+
   return (
-    <div>
-      <img src={recipe.strMealThumb} alt={recipe.strMeal} />
-      <h2>{recipe.strMeal}</h2>
-      <p>Страна: {recipe.strArea}</p>
-      <p>Категория: {recipe.strCategory}</p>
-      <p>Инструкция: {recipe.strInstructions}</p>
+    <div className={cl.recipe_wrapper}>
+      <img
+        className={cl.recipe_image}
+        src={recipe.strMealThumb}
+        alt={recipe.strMeal}
+      />
+      <div>
+        <h2 className={cl.recipe_title}>{recipe.strMeal}</h2>
+        <p className={cl.recipe_countrie}>Страна: {recipe.strArea}</p>
+        <p className={cl.recipe_category}>Категория: {recipe.strCategory}</p>
+        <p className={cl.recipe_instruction}>
+          Инструкция: {recipe.strInstructions}
+        </p>
+      </div>
+      <div>
+        <h3>Ингредиенты</h3>
+        <ul>
+          {ingredients.map((item, index) => (
+            <li ley={index}>
+              {item.ingredient} {item.measure}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
